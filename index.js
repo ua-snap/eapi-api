cd9fb4def7035842b6aa8b4260e6f9b4313e7340
Bruce Crevensten <bruce.crevensten@gmail.com>
2020-06-01 11:25:16 -0800

    Interim commit for debugging

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { validate, ValidationError, Joi } = require("express-validation");
 const { exec } = require("child_process");
+var hash = require('object-hash');
 
 const app = express();
 const port = 3000;
@@ -11,10 +12,19 @@ app.set('view engine', 'pug')
 app.use(express.static('public'))
 app.use(bodyParser.json()); // TODO may not need this after dev?
 
+// Wire in the validation code
+app.use(function(err, req, res, next) {
+    if (err instanceof ValidationError) {
+        return res.status(err.statusCode).json(err);
+    }
+
+    return res.status(400).json(err);
+});
+
 // Define the validation for parameters
 // Only doing the most basic validation at this point
 // to reduce security problems.
-const loginValidation = {
+const paramValidation = {
     query: Joi.object({
         analog_bbox_n: Joi.number(),
         analog_bbox_w: Joi.number(),
@@ -43,14 +53,63 @@ const loginValidation = {
         override_year_3: Joi.number(),
         override_year_4: Joi.number(),
         override_year_5: Joi.number(),
+        detrend_data: Joi.number(),
     }),
 };
 
 app.get(
     "/",
-    validate(loginValidation, { keyByField: true }, {}),
+    validate(paramValidation, { keyByField: true }, {}),
     (req, res) => {
-        exec("ncl -V", (error, stdout, stderr) => {
+
+        nonce = hash(req.query, { algorithm: "md5"} )
+
+        cli_string = `
+mkdir -p /tmp/${nonce} && \
+export NCL_OUTPUT_DIR=/tmp/${nonce}/ && \
+conda run -n ncl_stable ncl -n -Q \
+fromweb=1 \
+txtareayesno=2 \
+heightlev=1 \
+templev=1 \
+detrend=${req.query.detrend_data} \
+howmanyyears=${req.query.num_analogs} \
+seaiceyesno=0 \
+manualyesno=${req.query.manual_match} \
+manyear1=${req.query.override_year_1} \
+manyear2=${req.query.override_year_2} \
+manyear3=${req.query.override_year_3} \
+manyear4=${req.query.override_year_4} \
+manyear5=${req.query.override_year_5} \
+variable=${req.query.forecast_theme} \
+useryear=2017 \
+fmnths=1 \
+fmnths2=2 \
+slpwgt1=${req.query.manual_weight_1} \
+h500wgt1=${req.query.manual_weight_2} \
+t2mwgt1=${req.query.manual_weight_3} \
+t925wgt1=${req.query.manual_weight_4} \
+sstwgt1=${req.query.manual_weight_5} \
+icewgt1=100 \
+autoWgt=${req.query.auto_weight} \
+y1=${req.query.analog_bbox_s} \
+y2=${req.query.analog_bbox_n} \
+x1=${req.query.analog_bbox_w} \
+x2=${req.query.analog_bbox_e} \
+AK1=${req.query.forecast_bbox_s} \
+AK2=${req.query.forecast_bbox_n} \
+AK3=${req.query.forecast_bbox_w} \
+AK4=${req.query.forecast_bbox_e} \
+usermonth=1 \
+exmth=3 \
+justcorrelations=${req.query.correlation} \
+ID=12345 \
+specialnum=99 \
+/Users/becrevensten/repos/snap/eapi-analogs/SeaIceSelectPercents4bRMS.ncl
+`
+        cli_string = "./run_ncl.sh";
+        console.log(cli_string);
+        exec(cli_string, { shell: "/bin/bash" }, (error, stdout, stderr) => {
             if (error) {
                 console.error(`exec error: ${error}`);
                 return;
@@ -58,8 +117,9 @@ app.get(
             console.log(`stdout: ${stdout}`);
             console.error(`stderr: ${stderr}`);
         });
+
         // Here's where we'll dispatch out to the shell and trigger the command.
-        res.status(200).json(req.query);
+        res.send(cli_string);
     }
 );
 
@@ -75,13 +135,6 @@ app.get("/template", (req, res) => {
     })
 })
 
-app.use(function(err, req, res, next) {
-    if (err instanceof ValidationError) {
-        return res.status(err.statusCode).json(err);
-    }
-
-    return res.status(400).json(err);
-});
 
 app.listen(port, () =>
     console.log(`Example app listening at http://localhost:${port}`)
